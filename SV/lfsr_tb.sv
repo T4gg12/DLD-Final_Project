b580980a7a66c48337d243aa7630eebafb58663e
Zane_Francis <153263134+T4gg12@users.noreply.github.com>
2024-04-18 18:59:01 -0500

    update

--- SV/lfsr_tb.sv
+++ SV/lfsr_tb.sv
@@ -1,31 +1,31 @@
 // testbench to prove maximal LFSR
-module tb ();
+module stimulus ();
 
-   //logic variables to route input and output to DUT
-
-   //create file handles to write results to a file
+   logic [7:0] seed;
+   logic clk;
+   logic reset;
+   logic [7:0] shift_seed;
    
-   // instantiate device under test (small LFSR)
+   integer handle3;
+   integer desc3;
+   
+   lfsr dut(.seed(seed), .clk(clk), .reset(reset), .shift_seed(shift_seed));
 
-   //set up a clock signal
    always     
      begin
-	clk = 1; #1; clk = 0; #1;
+	    clk = 1; #1;
+      clk = 0; #1;
      end
    
    initial
      begin
-	//set up output file
-	//set up any book keeping variables you may want to use
-	//set up a starting seed.  What happens with all 0s?
-	//reset your DUT
-	//save the initial output of your DUT to compare with current output
-	//and see whenb you repeat
+      handle3 = $fopen("lsfr.out");
      end
 
    always @(posedge clk)
      begin
-		//output your results to a file
+    desc3 = handle3;
+    #5 $fdisplay(desc3, "%b", shift_seed);
      end
 
    // check results on falling edge of clk
