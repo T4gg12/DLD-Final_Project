d3f4a86312249b10ef2b3e38e57db87a4411e2c9
Zane_Francis <153263134+T4gg12@users.noreply.github.com>
2024-05-01 20:39:11 -0500

    commit

--- SV/callTo.sv
+++ SV/callTo.sv
@@ -1,22 +1,44 @@
-module callTo (input logic [63:0] seed,
-input logic clk,
-input logic reset,
-input logic sw1,
-input logic sw2,
-output logic [63:0] shift_seed);
+module callTo (
+    input logic [63:0] seed,
+    input logic clk,
+    input logic reset,
+    input logic sw1,
+    input logic sw2,
+    output logic [63:0] shift_seed
+);
 
+    logic [63:0] fsm_shift_seed;
+    logic [63:0] lfsr_shift_seed;
+    logic [63:0] dp_shift_seed;
+    logic fsmOut;
 
-logic [63:0] fsm_shift_seed;
-logic [63:0] lfsr_shift_seed;
-logic [63:0] dp_shift_seed;
-logic fsmOut;
+    FSM dut_fsm (
+        .clk(clk),
+        .reset(reset),
+        .switch1(sw1),
+        .switch2(sw2),
+        .seed(seed),
+        .shift_seed(fsm_shift_seed),
+        .out1(fsmOut)
+    );
 
-FSM dut(.clk(clk), .reset(reset), .switch1(sw1), .switch2(sw2), .seed(seed), .shift_seed(fsm_shift_seed), .out1(fsmOut));
+    lfsr dut_lfsr (
+        .clk(clk),
+        .reset(reset),
+        .seed(seed),
+        .shift_seed(lfsr_shift_seed)
+    );
 
-lfsr dut1(.clk(clk), .reset(reset), .seed(seed), .shift_seed(lfsr_shift_seed));
+    datapath dut_dp (
+        .grid(fsm_shift_seed),
+        .grid_evolve(dp_shift_seed)
+    );
 
-datapath dut2(.grid(fsm_shift_seed), .grid_evolve(dp_shift_seed));
-
-mux2 dut3(.d0(lfsr_shift_seed), .d1(dp_shift_seed), .s(fsmOut), .y(shift_seed)); 
+    mux2 #(64) dut_mux (
+        .d0(lfsr_shift_seed),
+        .d1(dp_shift_seed),
+        .s(fsmOut),
+        .y(shift_seed)
+    );
 
 endmodule
--- SV/clk_div.sv
+++ SV/clk_div.sv
@@ -1,13 +1,18 @@
-module clk_div (input logic clk, input logic rst, output logic clk_en);
+module clk_div (
+    input logic clk,
+    input logic rst,
+    output logic clk_en
+);
 
-   logic [27:0] clk_count; 
-   
-   assign clk_en = clk_count[27];
+    logic [27:0] clk_count; 
 
-   always_ff @(posedge clk) begin
-      if (rst)
-	clk_count <= 28'h0;
-      else
-   clk_count <= clk_count + 1;
-   end
+    always_ff @(posedge clk, posedge rst) begin
+        if (rst)
+            clk_count <= 63'h0;
+        else
+            clk_count <= clk_count + 1;
+    end
+
+    assign clk_en = clk_count[63];
+
 endmodule // clk_div
